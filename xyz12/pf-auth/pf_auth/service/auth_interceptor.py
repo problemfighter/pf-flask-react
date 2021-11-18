2776693cfd0b494be050adb16ec249d070142a67
hmtmcse <hmtm.cse@gmail.com>
2021-11-19 00:04:25 +0600

    added start with url

--- xyz12/pf-auth/pf_auth/service/auth_interceptor.py
+++ xyz12/pf-auth/pf_auth/service/auth_interceptor.py
@@ -3,8 +3,14 @@ from pf_flask.global_registry import get_global_app_config
 from pfms.pfapi.rr.pfms_request_respons import PfRequestResponse
 
 SYSTEM_DEFAULT_SKIP_URL = [
+    "/pf-swagger-ui",
     "/api/v1/operator/login",
-    "/api/v1/operator/init"
+    "/api/v1/operator/init",
+    "/pf-swagger-json"
+]
+
+SYSTEM_DEFAULT_SKIP_START_WITH_URL = [
+    "/pf-marshmallow-swagger/",
 ]
 
 
@@ -16,13 +22,19 @@ class AuthInterceptor(PfRequestResponse):
         skip_urls = SYSTEM_DEFAULT_SKIP_URL
         if get_global_app_config() and get_global_app_config().SKIP_URL_ON_AUTH:
             skip_urls += get_global_app_config().SKIP_URL_ON_AUTH
-        if relative_url not in skip_urls:
+        if relative_url not in skip_urls and not self.check_url_start_with(relative_url):
             return self.check_auth()
+
+    def check_url_start_with(self, request_url):
+        for url in SYSTEM_DEFAULT_SKIP_START_WITH_URL:
+            if request_url.startswith(url):
+                return True
+        return False
 
     def check_auth(self):
         bearer_token = pff_request_header_helper.get_bearer_token()
         if not bearer_token:
-            return self.error("You are not Authorize for Access.", http_code=401)
+            return self.error("You are not Authorize for Access.", code=4100, http_code=401)
 
 
 auth_interceptor = AuthInterceptor()
